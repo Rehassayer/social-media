dc1834a5236a309195da0ee3195b3117fd13a66b
Rehassayer <rakeshayer0@gmail.com>
2026-01-01 13:35:02 +0545

    cors added for the frontend connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,12 +7,19 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import likeRoutes from "./routes/likeRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 //API routes
 app.use("/", authRoutes);
